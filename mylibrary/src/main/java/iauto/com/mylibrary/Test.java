a1e5e2494f25718e95e56768aa17c1fd1f69ed32
caoquanli <caoquanli@pset.suntec.net>
2018-12-19 20:04:38 +0800

    add aar

---
+++ mylibrary/src/main/java/iauto/com/mylibrary/Test.java
@@ -0,0 +1,11 @@
+package iauto.com.mylibrary;
+
+import android.util.Log;
+
+public class Test {
+    private final static String TAG = "test";
+
+    public void print(String s) {
+        Log.d(TAG,"print : "+s);
+    }
+}
